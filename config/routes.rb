5fd08d55676e1c4d4c7d4858d5f84e1be9260188
Gilad Shanan <giladshanan@gmail.com>
2018-01-03 11:40:25 -0600

    update destroy games route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :games, only: [:index, :show, :new, :edit, :update]
+  resources :games, only: [:index, :show, :new, :edit, :update, :destroy]
   resources :photos, only: [:destroy]
   root to: "games#index"
 end

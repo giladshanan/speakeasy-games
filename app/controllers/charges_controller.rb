f523e84168bea04f6f1f4595e98f185f540a6fe2
Gilad Shanan <giladshanan@gmail.com>
2018-01-04 10:43:02 -0600

    update price on charges controller and success page

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,7 +4,7 @@ class ChargesController < ApplicationController
 
   def create
     # Amount in cents
-    @amount = 500
+    @amount = 2500
 
     customer = Stripe::Customer.create(
       :email => params[:stripeEmail],
@@ -14,7 +14,7 @@ class ChargesController < ApplicationController
     charge = Stripe::Charge.create(
       :customer    => customer.id,
       :amount      => @amount,
-      :description => 'Rails Stripe customer',
+      :description => 'Speakeasy Games customer',
       :currency    => 'usd'
     )
 

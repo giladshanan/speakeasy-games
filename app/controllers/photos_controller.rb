6eee80d59c5cf5e9ab48f0471c6abe9ab18ac89b
Gilad Shanan <giladshanan@gmail.com>
2018-01-04 09:43:41 -0600

    add routes, controller, views for Stripe API integration- two ways

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,17 +1,4 @@
 class PhotosController < ApplicationController
-
-  def index
-    # @games = Game.all
-  end
-
-  def show
-    # @game = Game.find(params[:id])
-  end
-
-  def edit
-    # @game = Game.find(params[:id])
-  end
-
   def destroy
     @photo = Photo.find( params[:id] )
     @game = @photo.game
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  root to: "games#index"
   resources :games, only: [:index, :show, :new, :edit, :update, :destroy]
   resources :photos, only: [:destroy]
-  root to: "games#index"
+  resources :charges
 end

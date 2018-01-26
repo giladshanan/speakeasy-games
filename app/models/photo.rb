581ac1780f1039ddf35cc0dae14a01190c95b9ad
Gilad Shanan <giladshanan@gmail.com>
2018-01-25 22:55:52 -0600

    configure paperclip to use s3 storage

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,6 @@
 class Photo < ApplicationRecord
   belongs_to :game
-  has_attached_file :img, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :img, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"#, adapter_options: { hash_digest: Digest::SHA256 }
   validates_attachment_content_type :img, content_type: /\Aimage\/.*\z/
 end
+

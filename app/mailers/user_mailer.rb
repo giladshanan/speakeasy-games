c2dd8ec4ee67065aa63fe0945c10ff5db79c223a
Gilad Shanan <giladshanan@gmail.com>
2018-01-27 11:16:51 -0600

    optimize email process with download link instead of attachments and delay job, secure email settings with SSL and 2fa app password

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,7 @@ class UserMailer < ApplicationMailer
     @user = user
     @order = order
     # @games = @order.games.pluck(:title)
-    attachments['SilverGrass.zip'] = File.read('public/SilverGrass.zip')
+    # attachments['SilverGrass.zip'] = File.read('public/SilverGrass.zip')
     mail(to: @user.email, subject: 'Your Recent Order From Speakeasy Games!')
   end
 end

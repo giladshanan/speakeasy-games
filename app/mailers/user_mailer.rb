24f57df247610d4a01062201a5caf608e7ab30a7
Gilad Shanan <giladshanan@gmail.com>
2018-01-27 10:34:24 -0600

    clean up mailer logic

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ApplicationMailer
   def welcome_email(user, order)
     @user = user
     @order = order
-    @games = @order.games.pluck(:title)
+    # @games = @order.games.pluck(:title)
     attachments['SilverGrass.zip'] = File.read('public/SilverGrass.zip')
     mail(to: @user.email, subject: 'Your Recent Order From Speakeasy Games!')
   end

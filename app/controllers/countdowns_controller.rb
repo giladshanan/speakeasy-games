1a9064850aeeeeb5928b535909225d4ea63f2b60
Gilad Shanan <giladshanan@gmail.com>
2018-04-13 18:07:40 -0400

    only one timer per purchased game, move button to profile page

--- app/controllers/countdowns_controller.rb
+++ app/controllers/countdowns_controller.rb
@@ -1,23 +1,22 @@
 class CountdownsController < ApplicationController
 
-  def new
-
-    render layout: false
-  end
-
   def create
-    @purchased_game = PurchasedGame.first
-    # @purchased_game = PurchasedGame.find(params[:purchased_game_id])
-    @countdown = Countdown.create(
-      purchased_game: @purchased_game,
-      total_time: 3,
-      initial_lockout: 5,
-      lockout_increment: 5,
-      guesses: 0,
-      code: "testingthecode",
-      )
-
-    redirect_to countdown_path(@countdown)
+    @purchased_game = PurchasedGame.find(params[:purchased_game_id])
+    if @purchased_game.countdown.present?
+      @countdown = @purchased_game.countdown
+      @alert = "You already have a countdown started."
+    else
+      @countdown = Countdown.create(
+        purchased_game: @purchased_game,
+        total_time: 3,
+        initial_lockout: 5,
+        lockout_increment: 5,
+        guesses: 0,
+        code: "testingthecode",
+        )
+      @alert = nil
+    end
+    redirect_to countdown_path(@countdown), alert: @alert
   end
 
   def show

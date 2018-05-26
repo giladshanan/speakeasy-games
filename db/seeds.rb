6108ccb0f484543b64441bb6e90a52e0de2968e0
Gilad Shanan <giladshanan@gmail.com>
2018-05-26 16:21:00 -0500

    correct typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@
 #   included: "Invitations; rules; detailed character notes for 17 different roles, including backstory, goals, and specific knowledge about other characters; printable paper props; decorating and site layout suggestions; detailed instructions for hosts; prop list",
 #   not_included: "Most physical materials, including character props",
 #   difficulty: "Medium",
-#   notes: "The hosts The Silver of Burnt Grass should expect to spend at least a couple hours tracking down the appropriate props. Some, but not all, of this responsibility can be passed on to the players. The Silver of Burnt Grass uses an optional dueling mechanic that improves the game, but requires a fair amount of set-up. Little to no decoration is required, but the game is better in a suitably decorated space.",
+#   notes: "The hosts of The Silver of Burnt Grass should expect to spend at least a couple hours tracking down the appropriate props. Some, but not all, of this responsibility can be passed on to the players. The Silver of Burnt Grass uses an optional dueling mechanic that improves the game, but requires a fair amount of set-up. Little to no decoration is required, but the game is better in a suitably decorated space.",
 #   price: 100,
 #   player_min: 16,
 #   player_max: 20,

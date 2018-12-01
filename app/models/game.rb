b0349a23f20ea5743d81e2e65cf1d601788c85d6
Gilad Shanan <giladshanan@gmail.com>
2018-12-01 13:01:19 -0600

    update gemfile, cleanup hash rocket

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,7 +4,7 @@ class Game < ApplicationRecord
   has_many :characters
   has_one  :cover_photo, dependent: :nullify
 
-  accepts_nested_attributes_for :testimonials, allow_destroy: true, :reject_if => lambda { |a| a[:description].blank? } # this prevents empty testimonials from getting saved
+  accepts_nested_attributes_for :testimonials, allow_destroy: true, reject_if: lambda { |a| a[:description].blank? } # this prevents empty testimonials from getting saved
 
   accepts_nested_attributes_for :characters, allow_destroy: true, reject_if: lambda { |a| a[:player_packet].blank? && a[:name].blank? } # this prevents empty characters from getting saved
 

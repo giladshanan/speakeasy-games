6e1ac518e000905672947baf239abe88d637a546
Gilad Shanan <giladshanan@gmail.com>
2018-12-01 23:45:33 -0600

    try again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,14 +76,14 @@ Rails.application.configure do
   # STMP settings for gmail
   config.action_mailer.smtp_settings = {
         :address          => "smtp.gmail.com",
-        :domain           => "speakeasy-games.herokuapp.com",
+        # :domain           => "speakeasy-games.herokuapp.com",
         :port             => 2525,
         :ssl              => true,
         :user_name        => ENV["GMAIL_USERNAME"],
         :password         => ENV["GMAIL_PASSWORD"],
-        :authentication   => "login",
+        :authentication   => "plain",
         :enable_starttls_auto => true,
-        :openssl_verify_mode => "none"
+        # :openssl_verify_mode => "none"
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
